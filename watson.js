32f7ca3e2d9190fee4c902a7613b55bb89083e6c
kennethrithvik <kennethrithvik@yahoo.in>
2018-02-26 12:05:58 +0800

    first

--- watson.js
+++ watson.js
@@ -1,61 +1,31 @@
-var watson = require('watson-developer-cloud');
+//var watson = require('watson-developer-cloud');
+var VisualRecognitionV3 = require('./node_modules/watson-developer-cloud/visual-recognition/v3');
 var fs = require('fs');
 var path = require('path');
 var Promise = require('bluebird');
 
-var speech_to_text = watson.speech_to_text({
-  username: <YOUR CREDS HERE>,
-  password: <YOUR CREDS HERE>,
-  version: 'v1',
-  url: 'https://stream.watsonplatform.net/speech-to-text/api',
+var visualRecognition = new VisualRecognitionV3({
+  api_key: '2cd9a7d2ba1456bb8cd4094d0e64561579c2fc69',
+  version_date: VisualRecognitionV3.VERSION_DATE_2016_05_20
 });
 
-exports.watsonSpeechToText = function(audioFile) {
+exports.watsonSpeechToText = function (audioFile) {
 
-  return new Promise(function(resolve, reject) {
+  return new Promise(function (resolve, reject) {
+    var recordingFile = fs.createWriteStream("results.json");
+    for (var i = 1; i < 5; i++) {
+      var imgFile="./images/tn_"+i+".png"
+      var params = {
+        images_file: fs.createReadStream(imgFile)
+      };
 
-    var params = {
-      content_type: 'audio/flac',
-      timestamps: true,
-      continuous: true
-    };
-
-    var results = [];
-
-    // create the stream
-    var recognizeStream = speech_to_text.createRecognizeStream(params);
-
-    // pipe in some audio
-    fs.createReadStream(audioFile).pipe(recognizeStream);
-
-    // listen for 'data' events for just the final text
-    // listen for 'results' events to get the raw JSON with interim results, timings, etc.
-
-    recognizeStream.setEncoding('utf8'); // to get strings instead of Buffers from `data` events
-
-    recognizeStream.on('results', function(e) {
-      if (e.results[0].final) {
-        results.push(e);
-      }
-    });
-
-    ['data', 'results', 'error', 'connection-close'].forEach(function(eventName) {
-      recognizeStream.on(eventName, console.log.bind(console, eventName + ' event: '));
-    });
-
-    recognizeStream.on('error', function(err) {
-      util.handleError('Error writing to transcript.json: ' + err);
-    });
-
-    recognizeStream.on('connection-close', function() {
-    	var transcriptFile = path.join(__dirname, 'transcript.json');
-
-      fs.writeFile(transcriptFile, JSON.stringify(results), function(err) {
+      visualRecognition.classify(params, function (err, res) {
         if (err) {
-          util.handleError(err);
+          console.log(err);
+        } else {
+          recordingFile.write(JSON.stringify(res) + ",");
         }
-        resolve();
-      });
-    });
-  });
+      })
+    }
+  })
 };
--- youtube.js
+++ youtube.js
@@ -3,32 +3,39 @@ var Promise = require('bluebird');
 var ffmpeg = require('fluent-ffmpeg');
 var path = require('path');
 
-exports.getYouTubeAudio = function(videoId){
-	return new Promise(function(resolve, reject){
-      // Install youtube_dl locally: brew install youtube-dl
-    youtube_dl = spawn('youtube-dl', ['--extract-audio', '--audio-format', 'mp3', '-o', 'file.%(ext)s', "http://www.youtube.com/watch?v=" + videoId]);
+exports.getYouTubeAudio = function (videoId) {
+  return new Promise(function (resolve, reject) {
+    // Install youtube_dl locally: brew install youtube-dl
+    youtube_dl = spawn('youtube-dl', ['-f', 'mp4', '-o', 'file.%(ext)s', videoId]);
 
-    youtube_dl.stdout.on('data', function(data){
+    youtube_dl.stdout.on('data', function (data) {
       console.log(data.toString());
     });
 
-    youtube_dl.stderr.on('data', function(data){
+    youtube_dl.stderr.on('data', function (data) {
       process.stderr.write(data);
     });
-
+    var duration;
     // brew install ffmpeg
-    youtube_dl.on('exit', function(){
-      var mp3File = path.join(__dirname, 'file.mp3');
+    youtube_dl.on('exit', function () {
+      var mp3File = path.join(__dirname, 'file.mp4');
       var flacFile = path.join(__dirname, 'file.flac')
-      ffmpeg(mp3File)
-        .output(flacFile)
-        .on('end', function(){
+      ffmpeg.ffprobe(path.join(__dirname, 'file.mp4'), function (err, metadata) {
+        duration=metadata.format.duration;
+        console.log(duration);
+      });
+      new ffmpeg(mp3File)
+        .takeScreenshots({
+          count: parseInt(5),
+        }, path.join(__dirname, 'images'), function (err) {
+          console.log('screenshots were saved')
+        })
+        .on('end', function () {
           resolve();
         })
-        .on('error', function(err){
+        .on('error', function (err) {
           reject(err);
-        })
-        .run();
+        });
     });
   });
 };
